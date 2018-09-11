bced8df4a923cdcff96821aa649aaf512c9f07c6
Loki <subtlepseudonym@gmail.com>
2018-09-11 00:59:08 -0400

    Add materialize-css, update app.js to use it

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import M from 'materialize-css';
 
-import logo from '../assets/logo.svg';
 import './app.css';
 
 class App extends Component {
+  componentDidMount() {
+    var sidebars = document.querySelector('.sidenav');
+    M.Sidenav.init(sidebars, null);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="body">
+        <ul id="slide-out" className="sidenav sidenav-fixed">
+          <li><a className="sidenav-close" href="#!">First Sidebar Link</a></li>
+          <li><a className="sidenav-close" href="#!">Second Sidebar Link</a></li>
+        </ul>
+        <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        <main>
+          <p>
+            Now we've got Materialize CSS working!
+          </p>
+        </main>
       </div>
     );
   }
