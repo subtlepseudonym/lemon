8183241bb338add01ff17738fb45218d6cd754e4
Loki <subtlepseudonym@gmail.com>
2018-09-10 21:29:35 -0400

    Organize src into basic directories

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import logo from '../assets/logo.svg';
+import './app.css';
 
 class App extends Component {
   render() {
